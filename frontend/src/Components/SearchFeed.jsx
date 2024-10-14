9be6eb3b409f290a90eb7639b2ba71d7df11abcf
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-10-14 19:38:31 +0530

    Videos, VideoDetail and SearchFeed implemented

---
+++ frontend/src/Components/SearchFeed.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "./";
+import { useParams } from "react-router-dom"; 
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+    .then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for : 
+        <span style={{ color: "#F31503" }}> {searchTerm} </span> videos
+      </Typography>
+      <Box display='flex'>
+      <Box sx ={{mr: { sm:'100px'}}} />
+      <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default SearchFeed;
---
+++ frontend/src/Components/VideoDetail.jsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import Button from "@mui/material/Button";
+
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { Videos, Loader } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  {/*const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
+  const [liked, setLiked] = useState(false);
+const [disliked, setDisliked] = useState(false);*/}
+  const { id } = useParams();
+
+  {/*const handleLike = () => {
+    if (!liked) {
+      setLikes(likes + 1);
+      setLiked(true);
+      if (disliked) {
+        setDislikes(dislikes - 1);
+        setDisliked(false);
+      }
+    } else {
+      setLikes(likes - 1);
+      setLiked(false);
+    }
+  };
+
+  const handleDislike = () => {
+    if (!disliked) {
+      setDislikes(dislikes + 1);
+      setDisliked(true);
+      if (liked) {
+        setLikes(likes - 1);
+        setLiked(false);
+      }
+    } else {
+      setDislikes(dislikes - 1);
+      setDisliked(false);
+    }
+  };*/}
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
+    );
+  }, [id]);
+
+  if (!videoDetail?.snippet) return <Loader />;
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
+
+  return (
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${id}`}
+              className="react-player"
+              controls
+            />
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: "#fff" }}
+              py={1}
+              px={2}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography
+                  variant={{ sm: "subtitle1", md: "h6" }}
+                  color="#fff"
+                >
+                  {channelTitle}
+                  <CheckCircleIcon
+                    sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+                  />
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} views
+                </Typography>
+               {/*<Button
+                  onClick={handleLike}
+                  variant="contained"
+                  color={liked ? "blue" : "black"}
+                >
+                  <ThumbUpIcon />
+                  Like {likes}
+                </Button>
+                <Button
+                  onClick={handleDislike}
+                  variant="contained"
+                  color={disliked ? "blue" : "black"}
+                >
+                  <ThumbDownIcon />
+                  Dislike {dislikes}
+  </Button>*/}
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
