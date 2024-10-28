a2d485b79bdf20a086d2632a60e405f9584cfcb0
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-10-29 00:06:49 +0530

    User dto created

---
+++ springboot-backend/src/main/java/com/vid_app/springboot_backend/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.vid_app.springboot_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    
+    private String userId;
+
+    private String username;
+
+    private String email;
+
+}
