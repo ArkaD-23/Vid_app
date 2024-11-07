b7ac3224c143cdbfed1fb10f26f07f54881f91b8
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-11-07 23:08:40 +0530

    Register functionality in backend

--- springboot-backend/src/main/java/com/vid_app/springboot_backend/controllers/UserController.java
+++ springboot-backend/src/main/java/com/vid_app/springboot_backend/controllers/UserController.java
@@ -1,9 +1,22 @@
 package com.vid_app.springboot_backend.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.vid_app.springboot_backend.dto.UserDto;
+import com.vid_app.springboot_backend.entities.User;
 import com.vid_app.springboot_backend.services.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -13,5 +26,25 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/register") 
+    public ResponseEntity<?> registration(@Valid @ModelAttribute("user") UserDto userDto, BindingResult result, Model model) {
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+        
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
+            return ResponseEntity.badRequest().body("The user is already registered !");
+        }
+
+        if (result.hasErrors()) {
+            List<String> errors = result.getAllErrors()
+                                        .stream()
+                                        .map(error -> error.getDefaultMessage())
+                                        .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errors);
+        }
+
+        userService.saveUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully !");
     }
 }
