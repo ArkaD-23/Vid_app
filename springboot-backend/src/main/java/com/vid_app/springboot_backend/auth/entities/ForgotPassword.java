bd7e506e93f19f61bce01826cc0b56b5571799ce
Arkaprava Dhar <raj2003dhar@gmail.com>
2024-11-09 23:44:51 +0530

    Forgot Password entity

---
+++ springboot-backend/src/main/java/com/vid_app/springboot_backend/auth/entities/ForgotPassword.java
@@ -0,0 +1,36 @@
+package com.vid_app.springboot_backend.auth.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ForgotPassword {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer fpid;
+
+    @Column(nullable = false)
+    private Integer otp;
+
+    @Column(nullable = false)
+    private Date expirationTime;
+
+    @OneToOne
+    private User user;
+    
+}
